566e7b5e18ca9088df9900bbc44d34088b66b7d4
Hassan Ahmed <hassan.ahmed@arpatech.com>
2017-05-09 19:23:38 +0500

    Still working on regex

--- regex.py
+++ regex.py
@@ -5,24 +5,31 @@ def find_all():
 	# Open file
 	f = open('chat.txt', 'r')
 	# Feed the file text into findall(); it returns a list of all the found strings
-	strings = re.findall(r'\d{1,2}/\d{1,2}/\d{2}, \d{1,2}:\d{2} (?:(?:P|A)M) - ((?:\w+ )*\w+):', f.read())
+	match = re.match(r'\d{1,2}/\d{1,2}/\d{2}, \d{1,2}:\d{2} (?:(?:P|A)M) - (?:\w+ )*\w+:', f.read())
 
-	print("Total messages exchanged: ", len(strings))
-
-	for string in strings:
-		print(string)
+	if match:
+		print("match: ", match.group(0))
 
 def single_find():
 	text = "9/13/16, 12:33 PM - Hanif Mianjee: <Media omitted>"
-	match = re.search(r'^\d{1,2}/\d{1,2}/\d{1,2}, \d{1,2}:\d{2} ((P|A)M) - (\w+\s)*\w+:', text)
+	match = re.match(r'^\d{1,2}/\d{1,2}/\d{1,2}, \d{1,2}:\d{2} (?:(?:P|A)M) - (?:\w+\s)*\w+:', text)
 	if match:
-		print("found: ", match.group())
+		print("found: ", match.group(1))
 
 def capture():
-	text = "aircraft and jetter"
-	matches = re.findall(r'(air(?:craft|plane)|jet)', text)
+	text = "aircraft and jet"
+	matches = re.findall(r'\b(air(?:craft|plane)|jet)\b', text)
 
 	for string in matches:
 		print(string)
 
-capture()
+def match():
+	text = "fruit=apple \n color=blue"
+	# match = re.search(r'[ \t]*(\w+)[ \t]*=[ \t]*(.+)', text)
+	match = re.search(r'\w+=.+', text)
+	if match:
+		print("found: ", match.group(1))	
+
+# single_find()
+find_all()
+# match()
