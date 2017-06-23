ffe30cfbc4621d23e5f39415f429af63677d0fef
Hassan Ahmed <hassan.ahmed@arpatech.com>
2017-06-23 16:08:59 +0500

    refactoring work

--- whatschat.py
+++ whatschat.py
@@ -45,77 +45,132 @@ def word_count(user, word):
 
 	return count
 
-while True:
-	try:
-		line = raw_input()
-		
-		match = re.match(r'(\d{1,2}/\d{1,2}/\d{2}, \d{1,2}:\d{2} (?:P|A)M) - ((?:\w+ )*\w+): (.+)', line)
-		
-		if match:
-			date = match.group(1)
-			name = match.group(2)
-			text = match.group(3)
+def group_messages(messages):
+	groups = {}
+	arr = []
+	previous = None
 
-			matchedUser = None
-
-			for user in users:
-				if user.name == name:
-					matchedUser = user
-
-			if matchedUser:
-				matchedUser.messageCount += 1
-
-			else:
-				matchedUser = User(name)
-				users.append(matchedUser)
-
-			msg = Message(date, matchedUser, text)
-			messages.append(msg)
-
-	except EOFError:
-		break
-
-print("Users : ", len(users))
-for user in users:
-	print("User ", user.name, " has sent ", user.messageCount, " messages.")
-	print("Media messages: ", message_count(user, 'media'), "Text messages: ", message_count(user, 'text'), "Links: ", message_count(user, 'link'))
-	# print("word used: ", word_count(user, "lol"))
-
-date1 = messages[0].date_time
-date2 = messages[-1].date_time
-duration = date2 - date1
-
-groups = {}
-arr = []
-previous = None
-
-for message in messages:
-	if len(arr) == 0:
-		arr.append(message)
-	else:
-		fmt = "%m/%d/%y, %I:%M %p"
-		time1 = datetime.strftime(previous.date_time, fmt)
-		time2 = datetime.strftime(message.date_time, fmt)
-		
-		diff = message.date_time - previous.date_time
-		# print("comparing ", time1, " with ", time2, "diff: ", diff)
-		if diff.total_seconds() < 3600:
+	for message in messages:
+		if len(arr) == 0:
 			arr.append(message)
 		else:
-			groups[datetime.strftime(previous.date_time, "%m/%d/%y, %I:%M %p")] = arr
-			arr = [message]
-	previous = message
-groups[datetime.strftime(previous.date_time, "%m/%d/%y, %I:%M %p")] = arr
+			fmt = "%m/%d/%y, %I:%M %p"
+			time1 = datetime.strftime(previous.date_time, fmt)
+			time2 = datetime.strftime(message.date_time, fmt)
+			
+			diff = message.date_time - previous.date_time
+			# print("comparing ", time1, " with ", time2, "diff: ", diff)
+			if diff.total_seconds() < 3600:
+				arr.append(message)
+			else:
+				groups[datetime.strftime(previous.date_time, "%m/%d/%y, %I:%M %p")] = arr
+				arr = [message]
+		previous = message
+	groups[datetime.strftime(previous.date_time, "%m/%d/%y, %I:%M %p")] = arr
 
-print("Group length: ", len(groups))
-# print("Group: ", groups)
+	return groups
 
-print("Total message count : ", len(messages))
+def group_description(groups):
+	max_minutes = 0
+	max_messages = 0
 
-print("Duration: ", duration.days)
+	for key,value in groups.items():
+		startTime = value[0].date_time
+		endTime = value[-1].date_time
 
-for key,value in groups.items():
-	fmt = "%m/%d/%y, %I:%M %p"
-	time1 = datetime.strftime(value[0].date_time, fmt)
-	time2 = datetime.strftime(value[-1].date_time, fmt)
-	print(key, "count: ", len(value), time1, time2)
+		fmt = "%m/%d/%y, %I:%M %p"
+		startTimeStr = datetime.strftime(startTime, fmt)
+		endTimeStr = datetime.strftime(endTime, fmt)
+		duration = endTime - startTime
+		
+		minutes = duration.seconds/60
+		message_count = len(value)
+
+		if max_minutes < minutes:
+			max_minutes = minutes
+
+		if max_messages < message_count:
+			max_messages = message_count
+
+		print("{} messages sent between {} and {} over a duration of {} minutes".format(message_count, startTimeStr, endTimeStr, minutes))
+
+	print("Maximum number of messages sent: {}".format(max_messages))
+	print("Maximum number of minutes: {}".format(max_minutes))
+
+def group_max(groups):
+	max_minutes = 0
+		max_messages = 0
+
+		for key,value in groups.items():
+			startTime = value[0].date_time
+			endTime = value[-1].date_time
+
+			fmt = "%m/%d/%y, %I:%M %p"
+			startTimeStr = datetime.strftime(startTime, fmt)
+			endTimeStr = datetime.strftime(endTime, fmt)
+			duration = endTime - startTime
+			
+			minutes = duration.seconds/60
+			message_count = len(value)
+
+			if max_minutes < minutes:
+				max_minutes = minutes
+
+			if max_messages < message_count:
+				max_messages = message_count
+
+	return (max_messages, max_minutes)	
+
+def main():
+	while True:
+		try:
+			line = raw_input()
+			
+			match = re.match(r'(\d{1,2}/\d{1,2}/\d{2}, \d{1,2}:\d{2} (?:P|A)M) - ((?:\w+ )*\w+): (.+)', line)
+			
+			if match:
+				date = match.group(1)
+				name = match.group(2)
+				text = match.group(3)
+
+				matchedUser = None
+
+				for user in users:
+					if user.name == name:
+						matchedUser = user
+
+				if matchedUser:
+					matchedUser.messageCount += 1
+
+				else:
+					matchedUser = User(name)
+					users.append(matchedUser)
+
+				msg = Message(date, matchedUser, text)
+				messages.append(msg)
+
+		except EOFError:
+			break
+
+	print("Total Users in this chat: ", len(users))
+	# for user in users:
+	# 	print("User ", user.name, " has sent ", user.messageCount, " messages.")
+	# 	print("Media messages: ", message_count(user, 'media'), "Text messages: ", message_count(user, 'text'), "Links: ", message_count(user, 'link'))
+		# print("word used: ", word_count(user, "lol"))
+
+	date1 = messages[0].date_time
+	date2 = messages[-1].date_time
+	duration = date2 - date1
+
+	groups = group_messages(messages)
+	group_description(groups)
+
+	print("Conversations: ", len(groups))
+	# print("Group: ", groups)
+
+	print("Total messages exchanged: ", len(messages))
+
+	print("Total days: ", duration.days)
+	print("Messages per day: ", len(messages)/duration.days)
+
+main()
